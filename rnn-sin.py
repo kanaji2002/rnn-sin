22477d594df16dbc1856e0285fc3a125e7826e40
13-kanaji <st23405@kagawa.kosen-ac.jp>
2023-09-19 11:09:33 +0900

    6.2

--- rnn-sin.py
+++ rnn-sin.py
@@ -1,4 +1,8 @@
 import torch.nn as nn
+import math
+import torch
+import matplotlib.pyplot as plt
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -9,3 +13,19 @@ class Net(nn.Module):
             hidden_size=64,
             batch_first=True,
         )
+        ...
+        
+    def forward(self, x):
+        ...
+
+        y_rnn, h=self.rnn(x,None)
+        ...
+        return y
+
+sin_x=torch.linspace(-2*math.pi, 2*math.pi, 100)
+sin_y=torch.sin(sin_x)
+plt.plot(sin_x, sin_y)
+plt.show()
+
+       
+               
