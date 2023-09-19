bb94ee9a9262568618168eb7766e233d70cf8e7f
13-kanaji <st23405@kagawa.kosen-ac.jp>
2023-09-19 10:50:45 +0900

    a

--- rnn-sin.py
+++ rnn-sin.py
@@ -1,1 +1,11 @@
-print(fs)
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ...
+        self.RNN(
+            input_size=1,
+            hidden_size=64,
+            batch_first=True,
+        )
