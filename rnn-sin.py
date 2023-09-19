fd8768ba3b487ef401ddf01b9636df11ee13ffad
13-kanaji <st23405@kagawa.kosen-ac.jp>
2023-09-19 12:12:39 +0900

    f

--- rnn-sin.py
+++ rnn-sin.py
@@ -2,7 +2,8 @@ import torch.nn as nn
 import math
 import torch
 import matplotlib.pyplot as plt
-
+from torch.utils.data import TensorDataset, DataLoader
+       
 
 class Net(nn.Module):
     def __init__(self):
@@ -27,5 +28,33 @@ sin_y=torch.sin(sin_x)
 plt.plot(sin_x, sin_y)
 plt.show()
 
-       
-               
+n_time=10
+n_sample=len(sin_x)-n_time
+
+input_data=torch.zeros((n_sample, n_time, 1))
+correct_data=torch.zeros((n_sample,1))
+for i in range(n_sample):
+    input_data[i]=sin_y[i:i+n_time].view(-1,1)
+    correct_data[i]=sin_y[i+n_time:i+n_time+1]
+    
+dataset=TensorDataset(input_data, correct_data)
+train_loder=DataLoader(dataset,batch_size=8,shuffle=True)
+
+class net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.rnn=nn.RNN(
+            input_size=1,
+            hidden_size=64
+            batch_first=True,
+        )
+        self.fc=nn.Linear(64,1)
+        
+    def forward(self, x):
+        y_rnn, h=self.rnn(x,None)
+        y=self.fc(y_rnn[:,-1,:])
+        return y
+
+net=Net()
+print(net)
+
